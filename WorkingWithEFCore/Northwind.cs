0e3a87cefec2cf2e99aa02778a0a03a47ce31de0
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2025-01-14 15:46:00 -0300

    Construindo modelos EF Core para as tabelas Northwind, Adicionando tabelas à classe de contexto do banco de dados Northwind

--- WorkingWithEFCore/Northwind.cs
+++ WorkingWithEFCore/Northwind.cs
@@ -4,6 +4,9 @@ namespace Packt.Shared;
 
 public class Northwind : DbContext
 {
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<Product>? Products { get; set; }
+
     protected override void OnConfiguring( DbContextOptionsBuilder optionsBuilder )
     {
         string path = Path.Combine(
@@ -14,5 +17,20 @@ public class Northwind : DbContext
         WriteLine($"Connection: {connection}");
         ForegroundColor = previousColor;
         optionsBuilder.UseSqlite(connection);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>()
+        .Property(category => category.CategoryName)
+        .IsRequired()
+        .HasMaxLength(15);
+
+        if (Database.ProviderName?.Contains("Sqlite") ?? false)
+        {
+            modelBuilder.Entity<Product>()
+            .Property(product => product.Cost)
+            .HasConversion<double>();
+        }
     }
 }
