40449860dbacab8640c85f74b13e4221d8f50a5f
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2025-01-14 16:53:31 -0300

    Filtrando logs por valores específicos do provedor

--- WorkingWithEFCore/Northwind.cs
+++ WorkingWithEFCore/Northwind.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace Packt.Shared;
 
@@ -7,35 +8,33 @@ public class Northwind : DbContext
     public DbSet<Category>? Categories { get; set; }
     public DbSet<Product>? Products { get; set; }
 
-    protected override void OnConfiguring( DbContextOptionsBuilder optionsBuilder )
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         string path = Path.Combine(Environment.CurrentDirectory, "Northwind.db");
-
         string connection = $"Filename={path}";
-        ConsoleColor previousColor = ForegroundColor;
-        ForegroundColor = ConsoleColor.DarkYellow;
 
-        WriteLine($"Connection: {connection}");
-
-        ForegroundColor = previousColor;
+        ConsoleColor previousColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine($"Connection: {connection}");
+        Console.ForegroundColor = previousColor;
 
         optionsBuilder.UseSqlite(connection);
 
-        optionsBuilder.LogTo(WriteLine).EnableSensitiveDataLogging();
+        optionsBuilder.LogTo(Console.WriteLine, new[] { RelationalEventId.CommandExecuting }).EnableSensitiveDataLogging();
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Category>()
-        .Property(category => category.CategoryName)
-        .IsRequired()
-        .HasMaxLength(15);
+            .Property(category => category.CategoryName)
+            .IsRequired()
+            .HasMaxLength(15);
 
         if (Database.ProviderName?.Contains("Sqlite") ?? false)
         {
             modelBuilder.Entity<Product>()
-            .Property(product => product.Cost)
-            .HasConversion<double>();
+                .Property(product => product.Cost)
+                .HasConversion<double>();
         }
     }
 }
