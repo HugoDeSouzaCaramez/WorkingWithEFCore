22dccd921cf608acd8409e5929bd7a12314250bb
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2025-01-14 16:46:53 -0300

    Logs EF Core

--- WorkingWithEFCore/Northwind.cs
+++ WorkingWithEFCore/Northwind.cs
@@ -9,14 +9,19 @@ public class Northwind : DbContext
 
     protected override void OnConfiguring( DbContextOptionsBuilder optionsBuilder )
     {
-        string path = Path.Combine(
-        Environment.CurrentDirectory, "Northwind.db");
+        string path = Path.Combine(Environment.CurrentDirectory, "Northwind.db");
+
         string connection = $"Filename={path}";
         ConsoleColor previousColor = ForegroundColor;
         ForegroundColor = ConsoleColor.DarkYellow;
+
         WriteLine($"Connection: {connection}");
+
         ForegroundColor = previousColor;
+
         optionsBuilder.UseSqlite(connection);
+
+        optionsBuilder.LogTo(WriteLine).EnableSensitiveDataLogging();
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
