7ec85cb7ba11197ca1ba2948f14f996320c8383d
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2025-01-14 15:12:17 -0300

    Conexão bem sucedida
    dotnet run

--- WorkingWithEFCore/Program.cs
+++ WorkingWithEFCore/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Packt.Shared;
+
+Northwind db = new();
+
+WriteLine($"Provider: {db.Database.ProviderName}");
